1f94e1204e25bc92abd7ee274f1893af51cad117
Mohammad Alshoubaki <mohammad.z.alshoubaki@gmail.com>
2020-10-30 09:42:41 -0400

    Add project files.

---
+++ XMLSerialization/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+namespace XMLSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+
+            emp.ID = 12;
+            emp.Name = "Moe";
+            emp.SSNumber = 123456789;
+            emp.EntryDate = DateTime.Now;
+
+            Console.WriteLine($"ID : {emp.ID} | Name : {emp.Name} | SSnumber : {emp.SSNumber} | Entry Date: {emp.EntryDate}");
+
+            string filePath = @"C:\Users\Moe\Desktop\";
+            string fileName = "Data.xml";
+
+            TextWriter writer = new StreamWriter(filePath + fileName);
+
+            XmlSerializer ser = new XmlSerializer(typeof(Employee));
+            ser.Serialize(writer, emp);
+            writer.Close();
+
+
+            Console.ReadLine();
+        }
+    }
+}
